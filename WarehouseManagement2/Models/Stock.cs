2354c3077928425c550e7595e47cb240ddd89e49
divyanshu15 <43715856+divyanshu15@users.noreply.github.com>
2021-04-29 13:08:00 +0530

    Add project files.

---
+++ WarehouseManagement2/Models/Stock.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WarehouseManagement2.Models
+{
+    public class Stock
+    {
+        public int Id { get; set; }
+        public string pName { get; set; }
+        public string pCategory { get; set; }
+        public string pPrice { get; set; }
+        public int pQty { get; set; }
+    }
+}
---
+++ WarehouseManagement2/Models/StockDbcontext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WarehouseManagement2.Models
+{
+    public class StockDbcontext : DbContext
+    {
+        public DbSet<Stock> Stocks { get; set; }
+
+        public StockDbcontext(DbContextOptions<StockDbcontext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Stock>().ToTable("Warehouse");
+        }
+    }
+}
